c59d8d83b2333e210bf3a7da5235367256db2293
grasshopper7 <grass.hopper.moc@gmail.com>
2019-09-26 10:25:49 +0530

    Add summary and progress plugins

--- cucumber-intro-junit/src/test/java/intro/OrderTest.java
+++ cucumber-intro-junit/src/test/java/intro/OrderTest.java
@@ -3,6 +3,8 @@ package intro;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "summary", "progress" })
 public class OrderTest {}
